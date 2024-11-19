aafb9a01807c8c97b46ccaf04ac0504ec1941a0e
Julia Bier <juliabiersuriano@gmail.com>
2024-11-19 10:51:34 -0400

    add request parsing and conditional response

--- app/server.go
+++ app/server.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"net"
 	"os"
 )
 
+type HTTPRequest struct {
+	Method      string
+	Target      string
+	HTTPVersion string
+}
+
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
@@ -33,13 +39,45 @@ func handleConnection(c net.Conn) {
 	defer c.Close()
 	log.Println("Accepted connection from: ", c.RemoteAddr())
 
-	// request will be interpreted here
-
-	res := []byte("HTTP/1.1 200 OK\r\n\r\n")
-
-	_, err := c.Write(res)
+	data := make([]byte, 1024)
+	_, err := c.Read(data)
 	if err != nil {
-		log.Printf("Error writing to %s connection: %s\n", c.RemoteAddr(), "err")
+		log.Printf("Error reading from %s connection: %s\n", c.RemoteAddr(), err)
 		return
 	}
+
+	req, err := parseRequest(data)
+
+	var res []byte
+	if req.Target == "/" {
+		res = []byte("HTTP/1.1 200 OK\r\n\r\n")
+	} else {
+		res = []byte("HTTP/1.1 404 Not Found\r\n\r\n")
+	}
+
+	_, err = c.Write(res)
+	if err != nil {
+		log.Printf("Error writing to %s connection: %s\n", c.RemoteAddr(), err)
+		return
+	}
+}
+
+func parseRequest(d []byte) (*HTTPRequest, error) {
+	request := new(HTTPRequest)
+
+	reqLine, _, hasCRLF := bytes.Cut(d, []byte("\r\n"))
+	if !hasCRLF {
+		return request, fmt.Errorf("Received malformed request.")
+	}
+
+	splitReqLine := bytes.Split(reqLine, []byte(" "))
+	if len(splitReqLine) != 3 {
+		return request, fmt.Errorf("Received malformed request line.")
+	}
+
+	request.Method = string(splitReqLine[0])
+	request.Target = string(splitReqLine[1])
+	request.HTTPVersion = string(splitReqLine[2])
+
+	return request, nil
 }
